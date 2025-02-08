6f9624e4310e5354e5bb8656a5c549efcf1c5543
RahimTS <rahimatme@gmail.com>
2025-02-08 11:29:34 +0530

    Changes for creating eureka server

--- src/main/java/rahim/learning/servicediscovery/ServiceDiscoveryApplication.java
+++ src/main/java/rahim/learning/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package rahim.learning.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class ServicediscoveryApplication {
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicediscoveryApplication.class, args);
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 
 }
